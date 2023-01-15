be7b7b29daa7e202bc04896f64f2baa0d4043c0b
林憶萱 <essen@gapp.nthu.edu.tw>
2023-01-16 02:24:45 +0800

    update folder name and upload some test data from tscs992

--- run.sh
+++
@@ -1,2 +0,0 @@
-python3.9 generate_database.py -n Essen -g woman -Y 1996 -M 7 -D 10 -o 15 -m 30
-python3.9 generate_database.py -n Husan -g woman -Y 1997 -M 7 -D 10 -o 15 -m 30
---
+++ src/run_tscs992.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import subprocess
+import sys
+ 
+path = '../input/tscs992_with_birth_day_randomized.csv'
+#所需的欄位
+usecols = ['id','v1','year', 's.mon', 's.day', 's.hour','s.min']
+df = pd.read_csv(path, usecols=usecols,encoding='Big5')
+# print(df)
+# print(df.head(10))
+
+data_size = len(df)
+print(data_size)
+for i in range(208,data_size):
+    print('id: ',str(df.at[i,'id']),'num: ',i)
+    if df.at[i,'v1'] == '男':
+        gender = 'm'
+    elif df.at[i,'v1'] == '女':
+        gender = 'w'
+    # print(df.at[i,'id'])
+    try:
+        subprocess.run(["python3.9","generate_database.py",
+        "-n",str(df.at[i,'id']),
+        "-g",gender,
+        "-Y",str(df.at[i,'year']),
+        "-M", str(df.at[i,'s.mon']), 
+        "-D" ,str(df.at[i,'s.day']), 
+        "-o", str(df.at[i,'s.hour']),
+        "-m" ,str(df.at[i,'s.min'])])
+    except:
+        print("Error: not finish ouput file: ",i,'th file','/ id: ',str(df.at[i,'id']))
+        sys.exit()
